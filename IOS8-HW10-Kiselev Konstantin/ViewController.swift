553234b5d83e0c472089afebe3503e7f5d0fec16
Konstantin <kostikem165@gmail.com>
2022-12-08 21:08:12 +0300

    Clean Code in Project

--- IOS8-HW10-Kiselev Konstantin/ViewController.swift
+++ IOS8-HW10-Kiselev Konstantin/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  IOS8-HW10-Kiselev Konstantin
-//
-//  Created by Константин Киселёв on 08.12.2022.
-//
+
 
 import UIKit
 
@@ -11,9 +6,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
