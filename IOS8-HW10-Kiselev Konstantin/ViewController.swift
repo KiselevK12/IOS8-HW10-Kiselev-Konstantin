56a25544df18749e7d6e7d513724cc6631687155
Konstantin <kostikem165@gmail.com>
2022-12-08 20:26:55 +0300

    Initial Commit

---
+++ IOS8-HW10-Kiselev Konstantin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS8-HW10-Kiselev Konstantin
+//
+//  Created by Константин Киселёв on 08.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
